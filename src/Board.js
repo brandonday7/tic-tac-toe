7b262673c0c464dc5b26a51e290c1a1ea8f947fd
Brandon Day <brandontday7@gmail.com>
2019-06-24 14:05:13 -0400

    completed algorithm to determine a winning board

--- src/Board.js
+++ src/Board.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import { isWin } from "./utils/utils.js"
 
 class Board extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			placement: [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+			placement: [[1, 1, 1], [0, 0, 0], [0, 0, 1]]
 		}
 	}
 
@@ -13,13 +14,14 @@ class Board extends Component {
   	const { placement } = this.state
     return (
       <div>
-      	{placement.map(row => (
-      		<div className="board-row">
-      			{row.map(snack => (
-      				<p>{snack}</p>
+      	{placement.map((row, i) => (
+      		<div className="board-row" key={i}>
+      			{row.map((snack, j) => (
+      				<p key={j}>{snack}</p>
       			))}
       		</div>
       	))}
+      	<h1>{isWin(placement)}</h1>
       </div>
     );
 
---
+++ src/utils/utils.js
@@ -0,0 +1,48 @@
+const isWin = placement => {
+	let winner = 0
+	for (let row = 0; row < placement.length; row++) {
+		for (let col = 0; col < placement[row].length; col++) {
+			const currentPlayer = placement[row][col]
+			if (currentPlayer) {
+				winner = findLine(placement, currentPlayer, row, col, 0)
+				if (winner) {
+					console.log("winner!", winner)
+					return winner
+				}
+				break
+			}
+		}
+		if (winner) break
+	}
+	return winner
+}
+
+const findLine = (placement, currentPlayer, row, col, count, caller) => {
+	if (count === placement.length - 1) {
+		console.log('thats a win')
+		return currentPlayer
+	} else {	
+		if (col < placement[row].length - 1 && placement[row][col + 1] === currentPlayer) {
+			console.log("to the right")
+			if (caller !== "r") count = 0
+			return findLine(placement, currentPlayer, row, col + 1, count + 1, "r")
+		} 
+		if (row < placement.length - 1 && placement[row + 1][col] === currentPlayer) {
+			console.log("below")
+			if (caller !== "b") count = 0
+			return findLine(placement, currentPlayer, row + 1, col, count + 1, "b")
+		} 
+		if (row < placement.length - 1 && col < placement[row].length - 1 && placement[row + 1][col + 1] === currentPlayer) {
+			console.log("diagonal right")
+			if (caller !== "dr") count = 0
+			return findLine(placement, currentPlayer, row + 1, col + 1, count + 1, "dr")
+		} 
+		if (col > 0 && row < placement.length - 1 && placement[row + 1][col - 1] === currentPlayer) {
+			console.log("diagonal left")
+			if (caller !== "dl") count = 0
+			return findLine(placement, currentPlayer, row + 1, col - 1, count + 1, "dl")
+		}
+	}
+}
+
+export { isWin }
